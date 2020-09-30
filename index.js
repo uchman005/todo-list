02f6fb78e392fe907aaacac893ab353e701990b6
uchman005 <obiefunamarcel@gmail.com>
2020-09-30 08:17:33 +0100

    Added style to my to-do list app.

--- index.js
+++ index.js
@@ -3,13 +3,31 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const port = 3000;
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/listsDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const listSchema = {
+name: String
+};
+
+const List =  mongoose.model("list", listSchema );
+
+const list = new List({
+  name: "New to-do list"
+});
+
+// list.save();
+List.find({name:"New to-do list"});
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.static('public'));
-let items = [];
+app.use(express.static(__dirname + '/public'));
+
+let items = ["Welcome to your to-do list", "Click the ➕ button to add new items", "Check the checkbox to remove to-dos"];
+
 app.get('/', (req, res) => {
   let today = new Date();
   let options = {weekday: 'long'};
@@ -17,14 +35,11 @@ app.get('/', (req, res) => {
   res.render('index', {dayOfWeek: day, newListItems: items})
 });
 
-
 app.post('/',(req, res)=> {
   let item = req.body.newItem;
   items.push(item);
-  console.log(items);
   res.redirect('/');
 });
-
 
 
 app.listen(port, ()=>console.log(`Server is live at port ${port}`));
